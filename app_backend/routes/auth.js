04e856eac951f7c34c1794331090fa740bf0af18
Nikhil Shinde <160251430+nikkkhilllit@users.noreply.github.com>
2024-12-29 17:50:08 +0530

    Update auth.js

--- app_backend/routes/auth.js
+++ app_backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {getToken} = require("../utils/helpers");
 
 // This POST route will help to register a user
